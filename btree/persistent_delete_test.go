3e51bf9acbb5eb74425143f36c8ff066093dfb4b
thetarby <furkanakyol1997@gmail.com>
2021-12-12 16:59:45 +0300

    * added deletion tests

---
+++ btree/persistent_delete_test.go
@@ -0,0 +1,63 @@
+package btree
+
+import (
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDelete_Should_Decrease_Height_Size_When_Root_Is_Empty_3(t *testing.T) {
+	log.SetOutput(ioutil.Discard)
+	tree := NewBtreeWithPager(4, NewNoopPagerWithValueSize(&PersistentKeySerializer{}, &SlotPointerValueSerializer{}))
+	for _, val := range []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10} {
+		tree.Insert(PersistentKey(val), SlotPointer{
+			PageId:  10,
+			SlotIdx: 10,
+		})
+	}
+	res, stack := tree.FindAndGetStack(PersistentKey(1), Insert)
+	assert.Len(t, stack, 3)
+	assert.Equal(t, SlotPointer{
+		PageId:  10,
+		SlotIdx: 10,
+	}, res.(SlotPointer))
+
+	tree.Delete(PersistentKey(1))
+	_, stack = tree.FindAndGetStack(PersistentKey(1), Insert)
+
+	assert.Len(t, stack, 2)
+}
+
+func TestPersistent_Deleted_Items_Should_Not_Be_Found(t *testing.T) {
+	log.SetOutput(ioutil.Discard)
+	tree := NewBtreeWithPager(4, NewNoopPagerWithValueSize(&PersistentKeySerializer{}, &SlotPointerValueSerializer{}))
+
+	n := 10000
+	for _, i := range rand.Perm(n) {
+		tree.Insert(PersistentKey(i), SlotPointer{
+			PageId:  int64(i),
+			SlotIdx: int16(i),
+		})
+		// println("inserted %v", num, i, tree.Height(), tree.pager.(*BufferPoolPager).pool.EmptyFrameSize())
+	}
+
+	for i := 0; i < n; i++ {
+		val := tree.Find(PersistentKey(i))
+		if val == nil {
+			tree.Find(PersistentKey(i))
+			tree.Print()
+		}
+		assert.Equal(t, SlotPointer{
+			PageId:  int64(i),
+			SlotIdx: int16(i),
+		}, val.(SlotPointer))
+		tree.Delete(PersistentKey(i))
+		// println("deleted %v", i)
+
+		val = tree.Find(PersistentKey(i))
+		assert.Nil(t, val)
+	}
+}
